cae7fb6cccde593248dc134f6c10d2da6c499c26
Beamlak Aschalew <birrletej12@gmail.com>
2025-03-15 18:08:19 +0300

    Add Sidebar components and lucide-vue-next dependency

---
+++ resources/js/Components/Sidebar/SidebarBody.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SidebarMenuItem from '@/Components/Sidebar/SidebarMenuItem.vue';
+</script>
+
+<template>
+    <nav
+        class="h-full overflow-y-auto [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 [&::-webkit-scrollbar]:w-2"
+    >
+        <div
+            class="hs-accordion-group flex w-full flex-col flex-wrap px-2 pb-0"
+            data-hs-accordion-always-open
+        >
+            <ul class="space-y-1">
+                <SidebarMenuItem path="/" active>Dashboard</SidebarMenuItem>
+                <SidebarMenuItem path="/genres">Genre</SidebarMenuItem>
+            </ul>
+        </div>
+    </nav>
+</template>
---
+++ resources/js/Components/Sidebar/SidebarMenuItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+interface Props {
+    active?: boolean;
+    path: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    active: false,
+});
+
+const itemStyle = computed(() => ({
+    'focus:outline-hidden flex w-full items-center gap-x-3.5 rounded-lg bg-gray-100 px-2.5 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700':
+        true,
+    'dark:bg-neutral-700 dark:text-white': props.active,
+    'dark:bg-neutral-800 dark:text-neutral-200': !props.active,
+}));
+</script>
+
+<template>
+    <li>
+        <a :class="itemStyle" href="#">
+            <slot></slot>
+        </a>
+    </li>
+</template>
