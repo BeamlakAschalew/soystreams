f798e84361463f635704fbd0888639b94953a6c5
Beamlak Aschalew <birrletej12@gmail.com>
2025-03-21 07:02:31 +0300

    add podcast search route to fetch and display results

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/podcasts', function () {
+    $client = new PodcastIndexWrapper\Client([
+        'app' => 'AppName',
+        'key' => 'RU3VQSRAMPQ6VAGUGEJK',
+        'secret' => 'xPahv6ZrqrNVEGFRmH8wq#PqhWM3nRCqgEZ8f5Jh'
+    ]);
+
+    $searchResult = $client->search->byTerm('meri');
+
+    dd($searchResult->json());
+    return $searchResult;
+});
+
 Route::get('/about', function () {
     return Inertia::render('About', [
         'title' => 'About'
