646dbb1ebbec935da43bcec0c9ea4c40295d44c1
Beamlak Aschalew <birrletej12@gmail.com>
2025-04-19 23:23:55 +0300

    feat: expand Station interface with additional properties for enhanced metadata

--- resources/js/Interfaces/Station.ts
+++ resources/js/Interfaces/Station.ts
@@ -3,4 +3,37 @@ export default interface Station {
     name: string
     url_resolved: string
     favicon: string
+    changeuuid: string
+    serveruuid: string
+    url: string
+    homepage: string
+    tags: string
+    country: string
+    countrycode: string
+    iso_3166_2: string
+    state: string
+    language: string
+    languagecodes: string
+    votes: number
+    lastchangetime: string
+    lastchangetime_iso8601: string
+    codec: string
+    bitrate: number
+    hls: number
+    lastcheckok: number
+    lastchecktime: string
+    lastchecktime_iso8601: string
+    lastcheckoktime: string
+    lastcheckoktime_iso8601: string
+    lastlocalchecktime: string
+    lastlocalchecktime_iso8601: string
+    clicktimestamp: string
+    clicktimestamp_iso8601: string
+    clickcount: number
+    clicktrend: number
+    ssl_error: number
+    geo_lat: number
+    geo_long: number
+    geo_distance: number | null
+    has_extended_info: boolean
 }
